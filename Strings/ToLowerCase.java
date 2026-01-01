8fbffdbf24edc793b150bebf843d52960647bb88
Snehal Kumbhar <snehal2634@gmail.com>
2026-01-01 18:26:24 +0530

    To Lower Case LeetCode 709 added

---
+++ Strings/ToLowerCase.java
@@ -0,0 +1,13 @@
+/*
+Leetcode Question Number = 709
+Name = To Lower Case
+Given String S
+Task is to return the string after replacing
+every uppercase letter with the same lowercase letter
+*/
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
