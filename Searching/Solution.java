b3d83e5c03effb29681085f94c287c62a688e744
Snehal Kumbhar <snehal2634@gmail.com>
2026-01-10 21:26:38 +0530

    Guess Number Higher or Lower LeetCode 374 added

---
+++ Searching/Solution.java
@@ -0,0 +1,31 @@
+/*
+Leetcode Question Number = 374
+Name = Guess Number Higher or Lower
+Given a number n, a number is picked between 1 and n (inclusive).
+You have to guess the picked number using a predefined API:
+int guess(int num)
+The API returns:
+-1 : if your guess is higher than the picked number  
+ 1 : if your guess is lower than the picked number  
+ 0 : if your guess is equal to the picked number  
+Task is to return the number that was picked.
+*/
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int s=1;
+        int e=n;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(guess(m)==-1){
+                e=m-1;
+            }
+            else if(guess(m)==1){
+                s=m+1;
+            }
+            else{
+                return m;
+            }
+        }
+        return -1;
+    }
+}
