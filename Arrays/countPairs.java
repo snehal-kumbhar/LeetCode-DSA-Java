a6f3383f620e9055e97a9f38164b457a07564942
Snehal Kumbhar <snehal2634@gmail.com>
2026-01-23 13:02:00 +0530

    Count Pairs Whose Sum is Less than Target LeetCode 2824 added

---
+++ Arrays/countPairs.java
@@ -0,0 +1,22 @@
+/* 
+Leetcode Question Number = 2824
+Name = Count Pairs Whose Sum is Less than Target
+Given a 0-indexed integer array nums of length n and an integer target
+Task is to return the number of pairs (i,j)
+where 0<=i<j<n and nums[i]+nums[j]<target.
+*/
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                int a=nums.get(i);
+                int b=nums.get(j);
+                if((a+b)<target){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
